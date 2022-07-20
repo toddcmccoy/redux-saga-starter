fdfdfb18e5dce5d34e29abf037e63b1df02000aa
Todd McCoy <93785112+toddcmccoy@users.noreply.github.com>
2022-07-20 09:23:35 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App.jsx';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
+const elementList = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_ELEMENTS':
+            return action.payload;
+        default:
+            return state;
+    }
+};    
+
+// this is the saga that will watch for actions
+function* watcherSaga() {
+
+}
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+// This is creating the store
+// the store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+    // This function is our first reducer
+    // reducer is a function that runs every time an action is dispatched
+    combineReducers({
+        elementList,
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App/>
+    </Provider>, 
+    document.getElementById('root')
+);
+
+registerServiceWorker();
